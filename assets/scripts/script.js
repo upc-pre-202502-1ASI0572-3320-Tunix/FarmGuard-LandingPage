8781bbccb71b686cb3863eb86a6b5a67d6fcf59e
Brayan Smith <brayanmq12@gmail.com>
2025-09-05 00:54:06 -0500

    chore: commit initial

---
+++ assets/scripts/script.js
@@ -0,0 +1,3 @@
+document.getElementById("action-button").addEventListener("click", function() {
+    window.location.href = "https://farmguard-frontend-915898677282.us-central1.run.app/auth/sign-in";
+});
